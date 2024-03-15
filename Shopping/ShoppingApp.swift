60df5c2790bdbf9747feeb1cd52711de3da2cbdc
Isuru-Manawadu <isurumanawadu1997@outlook.com>
2024-03-15 19:45:22 +0530

    Initial Commit

---
+++ Shopping/ShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingApp.swift
+//  Shopping
+//
+//  Created by Isuru Manawadu on 15/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
